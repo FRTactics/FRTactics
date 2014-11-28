10f6842dc94a6fac1ddd6cb7d0419386b146dd68
Arin <Arin@Reese-Laptop>
2014-11-28 13:19:32 -0500

    Changed code for in game GUI over to new layout

--- src/view/ingame/MainWindow.java
+++ src/view/ingame/MainWindow.java
@@ -4,34 +4,27 @@ package view.ingame;
 import controllers.screenControllers.ScrollPaneController;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import javax.swing.JFrame;
+import java.awt.Window;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+import model.GameManager;
 
-public class MainWindow
+public class MainWindow extends JPanel
 {
     public static Dimension windowSize;
     public static JScrollPane pane = new JScrollPane();
     
-    public static void createAndShowGUI()
+    public void createAndShowGUI()
     {
-        //Set up the JFrame
-        JFrame frame = new JFrame();
-        //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //frame.setUndecorated(true);
-        frame.setSize(800, 600);
-        frame.setVisible(true);
-        frame.setLayout(new BorderLayout());
-        frame.setTitle("Procedural Generator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Get the size of the windwow
-        windowSize = frame.getSize();
+        windowSize = this.getSize();
         //Make the Scroll Pane Visible
         pane.setVisible(true);
         //Create the game board
         DrawPanel dp = new DrawPanel(31);
         //add the scroll pane to the frame
-        frame.add(pane,BorderLayout.CENTER);
+        add(pane,BorderLayout.CENTER);
         //add the board to the scroll pane
         pane.setViewportView(dp);
         //remove the scroll bars
@@ -43,24 +36,15 @@ public class MainWindow
         dp.placeHexagons();
         //Create the glass pane and add it to the frame
         GlassPane glass = new GlassPane();
-        glass.setSize(frame.getWidth(),frame.getHeight());
-        frame.getRootPane().setGlassPane(glass);
-        frame.setGlassPane(glass);        
+        glass.setSize(getWidth(),getHeight());
+        getRootPane().setGlassPane(glass);        
         //create the pause menu
-        PauseDialog dialog = new PauseDialog(frame,glass);
+        PauseDialog dialog = new PauseDialog((Window)(GameManager.getInstance().getView().getParent()),glass);
         //Create the instance for statspopup
-        StatsPopup.createInstance(frame);
+        StatsPopup.createInstance((Window)(GameManager.getInstance().getView().getParent()));
         //add the listeners
-        frame.addKeyListener(new ScrollPaneController(pane.getViewport(),dp,frame,dialog));
-        pane.getVerticalScrollBar().addAdjustmentListener(event -> frame.repaint());
-        pane.getHorizontalScrollBar().addAdjustmentListener(event -> frame.repaint());
+        addKeyListener(new ScrollPaneController(pane.getViewport(),dp,this,dialog));
+        pane.getVerticalScrollBar().addAdjustmentListener(event -> repaint());
+        pane.getHorizontalScrollBar().addAdjustmentListener(event -> repaint());
     }
-    /*
-    public static void main(String [] agrs)
-    {
-        java.awt.EventQueue.invokeLater(() -> {
-            createAndShowGUI();
-        });
-    }
-    */
 }
